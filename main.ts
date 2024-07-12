e74527e21018cda6cb7d704d3e50538a48b1b5a6
BPM Circuits <65460644+bpmcircuits@users.noreply.github.com>
2024-07-12 16:08:23 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,1 @@
-VL6180X.init()
-let sensorID = VL6180X.getID()
-serial.writeLine("Sensor ID: " + sensorID)
-// Pause to show sensor ID
-basic.pause(2000)
-// Pause to show sensor ID
-basic.forever(function () {
-    let distance = VL6180X.readRange()
-if (distance != -1) {
-        serial.writeLine("Distance: " + distance + " mm")
-    } else {
-        serial.writeLine("Error reading distance")
-    }
-    basic.pause(100)
-})
+
